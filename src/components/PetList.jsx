ab9f69127775c1c281eeedd47a82ad52fad936b4
Haroon <you@example.com>
2024-08-12 15:52:40 +0300

    chore: Update sidebar layout and styling in App.css

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -6,20 +6,22 @@ const PetList = (props) => {
   ));
 
   return (
-    <div>
-      <h1>Pet List</h1>
-      {!props.petList.length ? (
-        <h2>No Pets Yet!</h2>
-      ) : (
-        <div>
-          <button onClick={props.handleFormView}>
-            {props.isFormOpen ? 'Close Form' : 'New Pet'}
-          </button>
-          <ul>{pets}</ul>
-        </div>
-      )}
-    </div>
+    // PetList.jsx
+
+<div className="sidebar-container">
+  <h1>Pet List</h1>
+  <div className="list-container">
+    {!props.petList.length ? (
+      <h2>No Pets Yet!</h2>
+    ) : (
+      <ul role="list">{pets}</ul>
+    )}
+  </div>
+  <button onClick={props.handleFormView}>
+    {props.isFormOpen ? 'Close Form' : 'New Pet'}
+  </button>
+</div>
   );
-};
+}
 
 export default PetList;
